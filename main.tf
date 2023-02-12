9e46a86a8c868c9590dc2ab82fc837ee48b5957a
sanket.joshi <sanket.joshi@relexsolutions.com>
2023-02-12 20:42:16 +0200

    test

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_service_plan" "example" {
 }
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/17"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
